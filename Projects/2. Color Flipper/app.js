75d5aa200c004d028fe9cb0e0f38a34ea57fc276
Joinas <jgoldhammer@outlook.de>
2024-02-27 12:53:52 +0100

    Update app.js

--- Projects/2. Color Flipper/app.js
+++ Projects/2. Color Flipper/app.js
@@ -1,3 +1,11 @@
 const colors = ["green", "blue", "rgba(133,162,360", "#f1502f"];
 
-const button = document.getElementById("btn")
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+// remember - you can write javascript functions inside something like this
+// also : what was the f -> syntax again?
+button.addEventListener('click',function(){
+    // checking if the body is targetted
+    // console.log(document.body);
+});
