6fcd8f23266e64a7fb9844465b0d5e5930e129b2
Joinas <jgoldhammer@outlook.de>
2024-02-28 16:25:34 +0100

    Add hexCode generator

---
+++ Projects/2. Color Flipper/hex.js
@@ -0,0 +1,24 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const button = document.getElementById("btn");
+const color =  document.querySelector(".color");
+
+button.addEventListener("click", function(){
+    const bgColor = getRandomHex();
+
+});
+
+function getRandomHex(){
+    // sequence contains 6 values of hex array
+    let hexSequence = "#";
+    for (let i = 0; i < 6; i++){
+        hexSequence += hex[getRandomHexElement()];
+    }
+    console.log(hexSequence);
+
+    return hexSequence;
+}
+
+function getRandomHexElement(){
+    return Math.floor(Math.random() * hex.length);
+}
