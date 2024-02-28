51c87319526ed8d5b913f6123b5a7a34160c7e4c
Joinas <jgoldhammer@outlook.de>
2024-02-28 15:37:00 +0100

    Add basic funcrion
    changing background to colors specified in the array

--- Projects/2. Color Flipper/app.js
+++ Projects/2. Color Flipper/app.js
@@ -5,7 +5,18 @@ const color = document.querySelector(".color");
 
 // remember - you can write javascript functions inside something like this
 // also : what was the f -> syntax again?
-button.addEventListener('click',function(){
+button.addEventListener("click",function(){
     // checking if the body is targetted
     // console.log(document.body);
+
+    // get random number between 0 and 3 
+    const randomNumber  = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+    // length is an attribute, not a function
+}
